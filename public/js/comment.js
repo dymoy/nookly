6c529bd4fb82c18cfb5c7fde4e19b5cb0fe4d560
Ryan Hanzel <100760416+H3Xivall@users.noreply.github.com>
2024-02-21 20:15:09 -0600

    Made some corrections to the addComment and homeRoutes file

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,31 +1,24 @@
 // Add Comments
 // Calls POST fetch to commentRoutes API route to add a comment to a listing
 
-// document.getElementById('add-comment').addEventListener('click', addComment);
-function addComment() {
-    const content = document.querySelector('#comment-content').value;
-    const listingId = document.querySelector('#listing-id').value;
+async function addComment(event) {
+  event.preventDefault();
 
-    fetch('/api/comment', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => {
-        if (response.ok) {
-            document.location.reload();
-        } else {
-            alert(response.statusText);
-        }
-    })
-    .catch(err => {
-        console.log(err);
-    })
-    .finally(() => {
-        document.querySelector('#comment-content').value = '';
-    })
+  // Get the value of the comment input
+  const comment = document.querySelector("#comment-content").value.trim();
+
+  // If the comment is not empty Fetch POST to add the comment to commentRoutes API
+  if (comment) {
+    const response = await fetch("/api/commentRoutes", {
+      method: "POST",
+      header: { "Content-Type": "application/json" },
+      body: JSON.stringify({ comment }),
+    }).then(() => {
+      document.location.reload();
+    });
+  } else {
+    alert("Please enter a comment before submitting");
+  }
 }
 
-module.exports = addComment;
+document.querySelector("#add-comment").addEventListener("submit", addComment);
