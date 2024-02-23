e0e94996d1251057498954ac8981ec3b8278f809
dymoy <148160802+dymoy@users.noreply.github.com>
2024-02-23 13:41:29 -0800

    Completed comment functionality. Restyled comments and new comment text box.

--- public/js/addcomment.js
+++ public/js/addcomment.js
@@ -1,30 +1,39 @@
-// Add Comments
-// Calls POST fetch to commentRoutes API route to add a comment to a listing
+/**
+ * @function addCommentFormHandler
+ * @param {*} event 
+ * 
+ * Handles the event that the user is adding a new comment to a listing
+ */
+async function addCommentFormHandler(event) {
+	event.preventDefault();
 
-async function addComment(event) {
-  event.preventDefault();
+	// Retrieve the comment text 
+	const content = document.getElementById('comment-content').value;
 
-  // Get the value of the comment input
-  const comment = document.getElementById("comment-content").value;
+	// Retrieve listing_id from window 
+	const listing_id = window.location.toString().split('/')[
+		window.location.toString().split('/').length - 1
+	];
 
-  // Retrieve the post ID from window
-  const listing_id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
-    ];
-  // If the comment is not empty Fetch POST to add the comment to commentRoutes API
-  if (comment) {
-    const response = await fetch("/api/commentRoutes", {
-      method: "POST",
-      header: { "Content-Type": "application/json" },
-      body: JSON.stringify({ comment, listing_id }),
-    }).then(() => {
-      document.location.reload();
-    });
-  } else {
-    alert("Please enter a comment before submitting");
-  }
+	// Validate that inputted comment is not empty 
+	if (content) {
+		// Call commentRoutes.js POST api route to create a new comment in the database
+		const response = await fetch('/api/comments', {
+			method: 'POST',
+			body: JSON.stringify({
+				content,
+				listing_id
+			}),
+			headers: {'Content-Type': 'application/json'}
+		});
+	
+		if (response.ok) {
+			// If the POST request was successful, reload the page to render the new added comment
+			document.location.reload();
+		} else {
+			alert(response.statusText);
+		}
+	}
 }
 
-document
-  .querySelector(".new-comment-form")
-  .addEventListener("submit", addComment);
+document.querySelector(".new-comment-form").addEventListener("submit", addCommentFormHandler);
