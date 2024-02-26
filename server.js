632e71ed0abb86e2298a3e152ef2b9b060e908cd
dymoy <148160802+dymoy@users.noreply.github.com>
2024-02-25 17:00:31 -0800

    Updated heroku config credentials.

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ const s3 = new S3Client({
         accessKeyId: process.env.AWS_ACCESS_KEY,
         secretAccessKey: process.env.AWS_SECRET_KEY
     },
-    region: process.env.AWS_REGION 
+    region: 'us-west-1'
 });
 
 const s3Storage = multerS3({
